5d9102a59a848f264e7dbbbbded3e7de2d3c03b2
R-Reichenbach <rafareichenbach@gmail.com>
2023-09-01 22:16:52 -0300

    modify

--- src/app/pages/password/password.component.ts
+++ src/app/pages/password/password.component.ts
@@ -2,31 +2,32 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PasswordModel } from 'src/app/models/PasswordModels';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
-  styleUrls: ['./password.component.scss']
+  styleUrls: ['./password.component.scss'],
 })
 export class PasswordComponent {
+  passwordForm!: FormGroup;
+  hide = true;
 
-  passwordForm! : FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {}
 
-  constructor(private formBuilder : FormBuilder,
-    private router: Router){ }
-
-  ngOnInit(): void{
-    this.passwordForm = this.formBuilder.group(
-    {
-      email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required]]
-    }
-    )
+  ngOnInit(): void {
+    this.passwordForm = this.formBuilder.group({
+      password: ['', [Validators.required]],
+    });
   }
 
-  submitPassword(){
-    debugger
+  submitPassword() {
+    debugger;
     var dadosPassoword = this.passwordForm.getRawValue() as PasswordModel;
   }
-
 }
